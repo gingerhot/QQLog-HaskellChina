503e84f3230119e594ce9d4952ba53d8b51419ef
B1nj0y <v@joyv.net>
2017-10-28 14:35:43 +0800

    update xserver

--- scripts/xserver.pl
+++ scripts/xserver.pl
@@ -8,28 +8,33 @@ use Encode qw(decode);
 
 # 设置我需要处理的群的群号
 my $group_number = 72874436;
+# 登录的QQ号
+my $qq = 1950606082;
 
-# 登录信息
-my $qq = 1950606082;           #登录的QQ号
-my $pwd = "your password";     #使用帐号密码方式登录时需要
-my $pwd_md5 = md5_sum($pwd);   #得到原始密码的32位长度md5
-
-#初始化一个客户端对象，设置登录的qq号
+# 初始化一个客户端对象，设置登录的qq号
 my $client = Mojo::Webqq->new(
-    ua_debug    =>  0,         #是否打印详细的debug信息
-    log_level   =>  "info",    #日志打印级别，debug|info|warn|error|fatal
-    qq          =>  $qq,       #必选，登录的qq帐号，用于帐号密码登录或保存登录cookie使用
-    pwd         =>  $pwd_md5,  #可选，如果选择帐号密码登录方式，必须指定帐号密码的md5值
-    login_type  =>  "qrlogin", #"qrlogin"表示二维码登录，"login"表示帐号密码登录
+    ua_debug    =>  0,      #是否打印详细的debug信息
+    log_level   =>  "info"  #日志打印级别，debug|info|warn|error|fatal
 );
 
-#注意: 腾讯可能已经关闭了帐号密码的登录方式，这种情况下只能使用二维码扫描登录
-#请关闭帐号的密保功能，不支持密保登录
-
-#客户端加载ShowMsg插件，用于打印发送和接收的消息到终端
+# 注意: 腾讯已经关闭了帐号密码的登录方式，这种情况下只能使用二维码扫描登录
+# 客户端加载ShowMsg插件，用于打印发送和接收的消息到终端
 $client->load("ShowMsg");
 
-#设置收发消息事件的回调函数
+# 登录过程如果需要手机扫描二维码，会将二维码以邮件附件的形式发送到指定邮箱，再通过手机QQ扫描二维码
+$client->load("PostQRcode", data=>{
+        smtp    =>  $ENV{'SMTP'},       #邮箱的smtp地址
+        port    =>  $ENV{'SMTP_PORT'},  #smtp服务器端口，默认25
+        from    =>  $ENV{'SENDER'},     #发件人
+        to      =>  $ENV{'RECEIVER'},   #收件人
+        user    =>  $ENV{'EMAIL_USER'}, #smtp登录帐号
+        pass    =>  $ENV{'EMAIL_PASS'}, #smtp登录密码
+        tls     =>  0,                  #可选，是否使用SMTPS协议，默认为0
+                                        #在没有设置的情况下，如果使用的端口为465，则该选项会自动被设置为1
+    }
+);
+
+# 设置收发消息事件的回调函数
 $client->on(
     receive_message => sub {
         my ($client,$msg) = @_;
@@ -43,8 +48,8 @@ $client->on(
     }
 );
 
-#ready事件触发时 表示客户端一切准备就绪：已经成功登录、已经加载完个人/好友/群信息等
-#你的代码建议尽量写在 ready 事件中
+# ready 事件触发时表示客户端一切准备就绪：已经成功登录、已经加载完个人/好友/群信息等
+# 你的代码建议尽量写在 ready 事件中
 $client->on(ready => sub{
     my $client = shift;
     #你的代码写在此处
